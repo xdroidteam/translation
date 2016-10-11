2bbab44d5906072546f2ca7bb52996c1c9ff085b
Mate Puskar <mate.puskar@gmail.com>
2016-10-11 13:30:06 +0200

    JS bug fix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -419,7 +419,7 @@
 
                 var $input = $this.next('input');
 
-                $.post("/translations/update-or-create", {
+                $.post(_xdroidTeamTranslationBaseRoute + "/update-or-create", {
                     locale: $input.data('locale'),
                     group: $input.data('group'),
                     key: $input.data('key'),
