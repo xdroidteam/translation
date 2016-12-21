33eee60c8fed9e9ef1fa87a8a3cf0fb0c1a9f434
Mate Puskar <mate.puskar@xdroid.com>
2016-12-21 22:24:14 +0100

    import command bug fix

--- src/Console/ImportCommand.php
+++ src/Console/ImportCommand.php
@@ -21,8 +21,10 @@ class ImportCommand extends Command {
 
             foreach (File::allFiles($basePath)  as $langFile) {
                 $group = explode('.', $langFile->getRelativePathname())[0];
-
                 foreach (array_dot(File::getRequire($langFile->getRealPath())) as $key => $translation) {
+                    if (is_array($translation))
+                        continue;
+
                     $this->createOrUpdate($lang, $group, $key, $translation);
                 }
                 Cache::tags('translations_' . env('APP_KEY'))->forget('translations.' . $lang . '.' . $group);
